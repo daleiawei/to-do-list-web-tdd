4165a59c1e412d379d40e36984ea93b02bc136a4
Wallace Zhang <daleiawei@gmail.com>
2016-12-14 09:52:00 -0500

    Functional test now checks we can input a do-do item

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common import keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,8 +18,31 @@ class NewVisitorTest(unittest.TestCase):
 
         #She noticed that both of the title and header include word "To-Do"
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
+        #App ask her to input a to do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
+
+        #She entered "Buy peacock feathers"
+        inputbox.send_keys('Buy peacock feathers')
+
+        #After she typed enter key, the page refreshed
+        #In the to-do list table there's new item displayed as "1: Buy peacock feathers"
+        inputbox.send_keys(keys.Enter)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+        
+        #Continue to input new to-do item
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore') 
